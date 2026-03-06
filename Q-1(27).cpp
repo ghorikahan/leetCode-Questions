83cdb74c6eed833f861574923f6edbe1adaa2359
ghorikahan <kahan.ghori.cg@gmail.com>
2026-03-06 19:02:12 +0530

    Q-27 is completed

---
+++ Q-1(27).cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    vector<int> nums = {0, 5, 0, 3, 6};
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+        {
+            return true;
+        }
+    }
+
+    return false;
+
+    return 0;
+}
