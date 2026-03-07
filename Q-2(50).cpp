5d6209197614499c8ff9ee260d0bddf3d36b688d
ghorikahan <kahan.ghori.cg@gmail.com>
2026-03-07 10:18:24 +0530

    completed 2nd question

---
+++ Q-2(50).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x = 2.1000;
+    int n = 3;
+    double res = pow(x, n);
+
+    return res;
+
+    return 0;
+}
